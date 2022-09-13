5bf3c675a8e028737d9a4c0a2549d11e41d707e4
Zahed <zahedm45@gmail.com>
2022-09-13 15:33:54 +0200

    Commented out the code that implies by day2 exercise.

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+function App() {
+    return(
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <header>Hello World2</header>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+
+
+    );
+}
+
+
+
+
+/*
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <header>Hello World2</header>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting userName={"Christian"}></Greeting>
+      <Greeting userName={"Christian2"}></Greeting>
     </div>
   );
 }
+const Greeting = ({userName}) => {
+  return (<h1>Hello, {userName}</h1>)
+}
+
+*/
+
+
 
 export default App;
+
+
+
+
